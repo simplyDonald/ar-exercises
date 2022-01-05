c1862a9081fe90b24d7e5de38b6613a65c40481b
simplyDonald <donaldabuah@gmail.com>
2022-01-05 19:24:51 +0000

    code cleanup

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,13 +5,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-# class ApplicationRecord < ActiveRecord::Base
-#   self.abstract_class = true
-# end
-
-# class Store < ActiveRecord::Base
-  
-# end
 
 burnaby = Store.create(name: "Burnaby", annual_revenue: 3000000, mens_apparel: :true, womens_apparel: :true)
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
