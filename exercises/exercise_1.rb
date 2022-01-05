bc88445a28398c0321b46a70c9b2a864a40a7b85
simplyDonald <donaldabuah@gmail.com>
2022-01-05 19:21:29 +0000

    setup store classes

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,19 @@
 require_relative '../setup'
 
+
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# class ApplicationRecord < ActiveRecord::Base
+#   self.abstract_class = true
+# end
+
+# class Store < ActiveRecord::Base
+  
+# end
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 3000000, mens_apparel: :true, womens_apparel: :true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Richmond", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts Store.all.count
